da93dab143a6a91fdee3b8ea3404bf279c3450a7
Aloxan12 <kvakvaaak@mail.ru>
2021-09-16 12:00:53 +0300

    added usersState

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const io = new Server(httpServer, {
         origin: "*",
     },
 });
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 3900;
 
 app.use(cors())
 app.get('/', ((req, res) => {
@@ -36,7 +36,10 @@ io.on("connection", (socketChannel) => {
         usersState.delete(socketChannel);
     })
 
-    socketChannel.on('client-message-sent', (name:string)=>{
+    socketChannel.on('client-name-sent', (name:string)=>{
+        if(typeof name !== 'string'){
+            return
+        }
         const user = usersState.get(socketChannel)
         user.name = name;
     })
